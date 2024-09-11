d0a2f1077650ac724499bd68d95abc8749f3390d
MayankPurohit8 <142736836+MayankPurohit8@users.noreply.github.com>
2024-09-11 15:13:08 +0000

    config variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=1232;
+let accoutEmail="hello@123"
+var accountPassword="12345"
+accountCity="doon"
+let accountState
+// accountId=2 not allowed
+console.log(accountId)
+//if variable is only iniatlized then it contained undefined
+console.log(accountState)
+console.table([accountId,accoutEmail,accoutEmail,accountCity])
+
+//never use var as it cause scope conflict
