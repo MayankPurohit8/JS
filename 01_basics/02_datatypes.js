df92c6c1c9084f3b203d891c06d799cd21fd2f79
MayankPurohit8 <142736836+MayankPurohit8@users.noreply.github.com>
2024-09-12 14:43:25 +0000

    dataypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as latest version
+
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+
+//number = integer upto 2^53
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol
+
+//object
+
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
